939058507e88a106e020278d1e7321a1783fb25d
Kaji Ryohji <kaji_ryohji@hotmail.com>
2018-08-21 17:12:58 +0200

    statistics finished

--- statistics.js
+++ statistics.js
@@ -1,13 +1,57 @@
 var members = data.results[0].members
 
-
 	
-
 	var Democrats = []
  	var Republicans = []
  	var Independents = []
 	
+	
+
+function Table(members) {
+     
+	var table = document.getElementById("senate-glance");
+	var tableBody= document.createElement("tBody");
+	tableBody.setAttribute("id","tBody")
+	
+	
+	for (var t = 0; t < members.length; t++) {
+		var row = document.createElement("tr");
+		
+		
+		var col1 = document.createElement("td");
+		col1.appendChild(col0);
+		
+		
+		var col2= document.createElement("td");
+		col2.textContent = Democrats
+			
+		
+		var col3 = document.createElement("td");
+		col3.textContent = Republicans
+			
+		
+		var col4  = document.createElement("td");
+		col4.textContent = Independents
+		
+	
+		row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
+        row.appendChild(col4);
+		tableBody.appendChild(row);
+		}
+	
+	table.appendChild(tableBody);
+
+}
+
+Table(members);
+
+
+
+	
 function countMembers(Arr1,letter){
+
 	for (var i = 0; i < members.length; i++) {
 		
 		if(members[i].party == letter){
@@ -15,13 +59,15 @@ function countMembers(Arr1,letter){
 		}
 	}
 }
-countMembers(Democrats,"D");
-countMembers(Republicans,"R");
-countMembers(Independents,"I");
+var numD =countMembers(Democrats,"D");
+var numR =countMembers(Republicans,"R");
+var numI =countMembers(Independents,"I");
 
 
 
 //contar cuantos hay en cada partido
+
+
 
 
 
@@ -50,19 +96,72 @@ function findAverageVotes (Array){
 	
 	
 	
-
+var sortedMembers = members;	
+function sortVotes(){
 	
-function leastOften(){
+sortedMembers.sort(function(a, b){return a.votes_with_party_pct - b.votes_with_party_pct});
+console.log(sortedMembers)
+}
+sortVotes();
+	
+// votos ordenadamente positivo//
+
+
+
+
+
+
+var sortedMembersInverse = members;
+console.log(sortedMembersInverse);
+
+function sortVotesInv(){
+	
+sortedMembersInverse.sort(function(a, b){return b.votes_with_party_pct - a.votes_with_party_pct});
+
+}
+//sortVotesInv();
+console.log(sortedMembersInverse)
+// votos ordenadamente negativo//
+
+
+
+
+	var least = []
+	var most = []
+
+function leastOften(ArrayVote,arraynew){
 	
 	var total = (members.length * 10)/100;
 	total= total.toFixed(0);
 	console.log(total);
 	
-for(var x=0; x<)
 	
+	
+	for(var x = 0; x < total; x++){
+		arraynew.push(ArrayVote[x])
+	}
+	console.log(arraynew);
+	
+	for(var z =0; z< ArrayVote.length; z++){
+		if(arraynew[arraynew.length-1].votes_with_party_pct === ArrayVote[z].votes_with_party_pct){
+			
+	if(arraynew.indexOf(ArrayVote[z]) ==  -1){
+		arraynew.push(ArrayVote[z])
+		}
+	}
+	}
+		console.log(arraynew);		
 }
+
+var least = leastOften(sortedMembers,least);
+var most = leastOften(sortedMembersInverse,most);
+
+
+
+
+ 
 	
-	
+
 	
 
 
@@ -71,12 +170,13 @@ for(var x=0; x<)
 
 var statistics = {
 	
-	"number_of_Democrats":0,
-	"number_of_Republicans":0,
-	"number_of_Independents":0,
+	"number_of_Democrats":numD,
+	"number_of_Republicans":numR,
+	"number_of_Independents":numI,
 	"average_Vote_With_Party_Demo":demo_ptc,
 	"average_Vote_With_Party_Repu":repu_ptc,
-	"members_Who_Least_Often_Vote_with_Their_Party":0,
+	"members_Who_Least_Often_Vote_with_Their_Party":least,
+	"members_Who_Most_Often_Vote_with_Their_Party":most,
 	"missed_Votes":0,
 	"members_Missed_Votes": 0,
 	"members_Do_Not_Vote_With_Their_Party":0,
